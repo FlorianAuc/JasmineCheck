8ef39e9075c8a54a3a90ef8f088a44bf06358cfb
FlorianAuc <florian.aucouturier@gmail.com>
2022-03-17 14:19:47 +0100

    finish

--- 3.2.Test2/questions.js
+++ 3.2.Test2/questions.js
@@ -1,43 +1,61 @@
 let CreationTableauLangages =  () => {
+   return langages = ["Html", "CSS", "Java", "PHP"];
 
 }
 
 let CreationTableauNombres =  () => {
+    return nombres = [0, 1, 2, 3, 4, 5]
 
 }
 
 let RemplacementElement =  (langages) => {
+    langages[2] = 'Javascript';
+    return langages;
 
 }
 
 let AjoutElementLangages =  (langages) => {
+    langages[4] = 'Ruby'
+    langages[5] = 'Python'
+    return langages
 
 }
 
 let AjoutElementNombres =  (nombres) => {
+    nombres.unshift(-2, -1);
+    return nombres
 
 }
 
 let SuppressionPremierElement =  (langages) => {
+    langages.shift();
+    return langages;
 
 }
 
 let SuppressionDernierElement =  (langages) => {
+    langages.pop();
+    return langages;
+
 
 }
 
 let ConversionChaineTableau =  (reseaux_sociaux_chaine) => {
+    return reseaux_sociaux_chaine.split(',');
 
 }
 
 let ConversionTableauChaine =  (langages) => {
+    return langages.toString();
 
 }
 
 let TriTableau =  (reseaux_sociaux) => {
+    return reseaux_sociaux.sort()
 
 }
 
 let InversionTableau =  (langages) =>{
+    return langages.reverse();
 
 }
